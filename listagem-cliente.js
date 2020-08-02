ac37701475060ffcb58dc80e5553a8f99994bfff
Wellington Roth <wellington_roth@hotmail.com>
2020-08-02 18:23:17 -0300

    editar cliente

--- listagem-cliente.js
+++ listagem-cliente.js
@@ -1,19 +1,23 @@
 const removeCliente = id => {
-  if(confirm("Deletar o cliente?"))
+  if(confirm("Deseja deletar o cliente ?"))
   deletaCliente(id)
+  document.location.reload();
 }
 
 const exibeCliente = (cpf, nome, id) => {
   const linha = document.createElement("tr");
 
-  const conteudoLinha = `
-    <td>${cpf}</td>
-    <td>${nome}</td>
-    <button type="button" class="btn btn-danger"
-    onclick="removeCliente(${id})">Excluir</button>
-  `;
+  const conteudoLinha = ` 
       
-  //o innerHTML  a inserindo uma linha para cada conteudo
+      <td>${cpf}</td>
+      <td>${nome}</td>
+      <button type="button" class="btn btn-danger"
+      onclick="removeCliente(${id})">Excluir</button>
+      <a href="edita-clientes.html?id=${id}">
+        <button type="button" class="btn btn-info">Editar</button>
+      </a>
+  `;
+
   linha.innerHTML = conteudoLinha;
 
   return linha;
